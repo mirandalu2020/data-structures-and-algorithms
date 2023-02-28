24b53ee8e13ec9a2e46471a05b4a36c50790053f
mirandalu2020 <yuanling1007@hotmail.com>
2023-02-27 17:20:15 -0800

    stretch goal completed

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -147,6 +147,22 @@ Return the resulting output array.
 
 const fizzbuzz = (arr) => {
   // Solution code here...
+  const fizzbuzzArr = [];
+  arr.forEach(element => {
+    if (element%5 === 0 && element%3 ===0) {
+      fizzbuzzArr.push('Fizz Buzz');
+    }
+    else if (element%5 === 0) {
+      fizzbuzzArr.push('Buzz');
+    }
+    else if (element%3 === 0) {
+      fizzbuzzArr.push('Fizz');
+    }
+    else {
+      fizzbuzzArr.push(element);
+    }
+  });
+  return fizzbuzzArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -200,7 +216,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
