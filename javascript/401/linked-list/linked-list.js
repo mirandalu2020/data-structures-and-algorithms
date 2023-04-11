c14633703738c913ace564aeb5947157916e6c51
mirandalu2020 <yuanling1007@hotmail.com>
2023-04-10 21:09:45 -0700

    insersion linked list comp

--- javascript/401/linked-list/linked-list.js
+++ javascript/401/linked-list/linked-list.js
@@ -11,6 +11,7 @@ class LinkedList {
 
   constructor() {
     this.head = null;
+    this.length=0;
   }
 
   insertVal(value) {
@@ -55,6 +56,67 @@ class LinkedList {
       }
     }
     return str;
+  }
+
+  append(val) {
+    let node = new Node(val);
+    if (this.head === null) {
+      this.head = node;
+      this.length += 1;
+    }
+    else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = node;
+      this.length += 1;
+    }
+  }
+
+  insertBefore(val, newVal) {
+    let node = new Node(newVal);
+    let current = this.head;
+    let previous = this.head;
+    let temp;
+
+    if (this.head.value ===val ) {
+      node.next = this.head;
+      this.head = node;
+    }
+
+    while(current.value !== val) {
+      temp = current;
+      current = current.next;
+      previous = temp;
+    }
+    //existing while-loop when current.value = val
+    previous.next = node;
+    node.next = current.next;
+    //move the value from value and beyond forward by 1 position
+
+  }
+
+  insertAfter(val, newVal) {
+    let node = new Node(newVal);
+    let current = this.head;
+    let after = this.head.next;
+    let temp;
+
+    while(current !== null) {
+      if (current.value !== val) {
+        temp=current;
+        current = after;
+        after = temp;
+      }
+      else {
+        current.next = node;
+        node.next = after;
+        break;
+      }
+
+    }
+
   }
 
 }
