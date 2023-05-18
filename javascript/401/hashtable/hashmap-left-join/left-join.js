2904a9d6af658add430b844e5feb37fca98c196a
mirandalu2020 <yuanling1007@hotmail.com>
2023-05-18 06:46:41 -0700

    left join tests added

--- javascript/401/hashtable/hashmap-left-join/left-join.js
+++ javascript/401/hashtable/hashmap-left-join/left-join.js
@@ -5,15 +5,19 @@ function leftJoin(synonyms, antonyms) {
   let leftJoin = {};
 
   for (let idx=0; idx<synonyms.size; idx++){
-    let commonKey = Object.keys(synonyms.buckets[idx])[0];
-    if (antonyms.has(commonKey)) {
-      let synonym = synonyms.get(commonKey);
-      let antonym = antonyms.get(commonKey);
-      leftJoin[commonKey] = [synonym, antonym];
-    }
-    else if (antonyms.has(commonKey) === false){
-      let synonym = Object.keys(synonyms[idx])[0];
-      leftJoin[synonym] = [synonyms[idx].synonym, null];
+    if (synonyms.buckets[idx]){
+      console.log(synonyms.buckets[idx].head.value);
+      let commonKey = Object.keys(synonyms.buckets[idx].head.value)[0];
+      if (antonyms.has(commonKey)) {
+        let synonym = synonyms.get(commonKey);
+        let antonym = antonyms.get(commonKey);
+        leftJoin[commonKey] = [synonym, antonym];
+      }
+      else if (antonyms.has(commonKey) === false){
+        let synonym = Object.keys(synonyms.buckets[idx].head.value)[0];
+
+        leftJoin[synonym] = [ synonyms.get(synonym), null];
+      }
     }
   }
 
